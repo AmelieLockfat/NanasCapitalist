1232f1c77220eaa5a9fbadc5e82def4b260fa4d1
AmelieLockfat <amelie.lock-fat@etud.univ-jfc.fr>
2024-01-23 17:07:06 +0100

    création resolvers

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
+let world = require("./world")
 const express = require('express');
 const { ApolloServer, gql } = require('apollo-server-express');
 // Construct a schema, using GraphQL schema language
 const typeDefs = require("./schema");
 // Provide resolver functions for your schema fields
-const resolvers = {
-    Query: {
-        hello: () => 'Hello world!',
-    },
-};
-const server = new ApolloServer({ typeDefs, resolvers})
+const resolvers =  require("./resolvers")
+const server = new ApolloServer({
+    typeDefs, resolvers,
+    context: async ({ req }) => ({
+        world: world
+    })
+});
+
 const app = express();
 app.use(express.static('public'));
 server.start().then( res => {
