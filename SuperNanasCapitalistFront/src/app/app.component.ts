2c2dd90e9fcab28848db2b0608c4082d82a8ffb7
AmelieLockfat <amelie.lock-fat@etud.univ-jfc.fr>
2024-01-24 14:01:59 +0100

    Quelques modifs côté serveur

--- SuperNanasCapitalistFront/src/app/app.component.ts
+++ SuperNanasCapitalistFront/src/app/app.component.ts
@@ -18,7 +18,7 @@ world: World = new World();
 constructor(private service: WebserviceService) {
   service.getWorld().then(
     world => {
-      this.world = world.data.getWorld;
+      this.world = world.data.getWorld();
     });
 }
 }
--- index.js
+++ index.js
@@ -2,15 +2,29 @@ let world = require("./world")
 const express = require('express');
 const { ApolloServer, gql } = require('apollo-server-express');
 // Construct a schema, using GraphQL schema language
+
+async function readUserWorld(user) {
+    try {
+        const data = await fs.readFile("userworlds/"+ user +
+            "-world.json");
+        return JSON.parse(data);
+    }
+    catch(error) {
+        return world
+    }
+}
+
 const typeDefs = require("./schema");
 // Provide resolver functions for your schema fields
 const resolvers =  require("./resolvers")
 const server = new ApolloServer({
     typeDefs, resolvers,
     context: async ({ req }) => ({
-        world: world
+        world: await readUserWorld(req.headers["x-user"]),
+        user: req.headers["x-user"]
     })
 });
+
 
 const app = express();
 app.use(express.static('public'));
