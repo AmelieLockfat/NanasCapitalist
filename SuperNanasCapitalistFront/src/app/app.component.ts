3b24f07194d405b1480e77ca5a5799e8c5c4558e
AmelieLockfat <amelie.lock-fat@etud.univ-jfc.fr>
2024-01-28 18:36:16 +0100

    j'ai réussi à mettre le sacré input

--- SuperNanasCapitalistFront/src/app/app.component.ts
+++ SuperNanasCapitalistFront/src/app/app.component.ts
@@ -3,22 +3,25 @@ import { RouterOutlet } from '@angular/router';
 import {World,Palier,Product} from "../../../world";
 import {WebserviceService} from "./webservice.service";
 import {ProductComponent} from "./product/product.component";
+import {NgModule} from "@angular/core";
+import { FormsModule } from '@angular/forms';  // Importez le module FormsModule
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProductComponent],
+  imports: [RouterOutlet, ProductComponent,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
+
 export class AppComponent {
   title = 'SuperNanasCapitalistFront';
-
+username : string ='';
 world: World = new World();
 api: String ='https://isiscapitalistgraphql.kk.kurasawa.fr/graphql'
 constructor(private service: WebserviceService) {
-
   service.getWorld().then(
     world => {
       this.world = world.data.getWorld;
@@ -26,4 +29,13 @@ constructor(private service: WebserviceService) {
     });
 
 }
+getUsername(){
+  if  (localStorage.getItem("username")){
+  this.username = localStorage.getItem("username")!;
+}}
+
+  onUsernameChanged(){
+  localStorage.setItem("username", this.username);
 }
+}
+
