68cd9edaefd89b4262331e1e8d9e7314e1dfec5a
lucilledelbos <lucilledelbos@etud.univ-jfc.fr>
2024-04-01 13:31:56 +0200

    mieux

--- SuperNanasCapitalistFront/src/app/bigvalue.pipe.ts
+++ SuperNanasCapitalistFront/src/app/bigvalue.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class BigvaluePipe implements PipeTransform {
 
-  transform(value: number, args?: any): string  {
+  transform(value: number, args?: any): string {
     let result: string = "";
 
     if (value < 1000)
@@ -14,10 +14,11 @@ export class BigvaluePipe implements PipeTransform {
     else if (value < 1000000)
       result = '$' + value.toFixed(0);
     else if (value < 1000000000)
-      result = '$' + (value / 1000000).toFixed(2) + " million";
-    else if (value >= 1000000000)
-      result = '$' + (value / 1000000000).toFixed(2) + " billion";
+      result = Math.floor(value / 1000000) + " million";
+    else
+      result = Math.floor(value / 1000000000) + " billion";
 
-      return result;  }
+    return result;
+  }
 }
 
